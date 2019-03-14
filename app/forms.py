56533599761e90d2fcba9372fe2df946f72d14c7
Vitaly Kalinin <xcrsx@icloud.com>
2019-03-14 11:01:42 +0500

    The main page, profile, search and sign in

--- app/forms.py
+++ app/forms.py
@@ -6,14 +6,14 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember')
     submit = SubmitField('Sign In')
 
 
 class MusicSearchForm(FlaskForm):
     choices = [('Artist', 'Artist'),
-                ('Album', 'Album')]
+               ('Album', 'Album')]
     select = SelectField(choices=choices)
     search = StringField("I'm looking for...", validators=[DataRequired()])
     submit = SubmitField('Find it!!!')
--- app/routes.py
+++ app/routes.py
@@ -6,19 +6,24 @@ from app.forms import LoginForm, MusicSearchForm
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = {'username': 'mock'}    
+    user = {'username': 'mock'}
     posts = [  # it will be the popular and new music
         {
-            'author': {'album': 'John'},
-            'body': 'Some text'
+            'author': {'name': 'John'},
+            'album': 'Some title'
         },
         {
-            'author': {'album': 'Susan'},
-            'body': 'another text'
+            'author': {'name': 'Susan'},
+            'album': 'another title'
         }
     ]
-    form = MusicSearchForm()
-    return render_template('index.html', title='Wharf', user=user, posts=posts, form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('index.html', title='Wharf', user=user, posts=posts,
+                           form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,3 +44,10 @@ def search():
            form.search.data))
         return redirect('/index')
     return render_template('search.html', title="Let's find smth", form=form)
+
+
+@app.route('/profile')
+def profile():
+    user = {'username': 'mock'}
+    return render_template('profile.html', title='My profile', user=user)
+

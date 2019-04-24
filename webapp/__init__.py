b00beef06ed4f5f4184745c5d2cae7bb89edf047
Vitaly Kalinin <xcrsx@icloud.com>
2019-04-24 13:34:27 +0500

    the first merge Changed DB: SQLite -> PostgreSQL

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,23 +2,37 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from webapp.parsing.views import blueprint as parsing_blueprint
-from webapp.user.views import blueprint as user_blueprint
-from webapp.search.views import blueprint as search_blueprint
-from webapp.db import db
 from flask_bootstrap import Bootstrap
 
+from webapp.db import db
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db.init_app(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'user.login'
-bootstrap = Bootstrap(app)
-app.register_blueprint(parsing_blueprint)
-app.register_blueprint(search_blueprint)
-app.register_blueprint(user_blueprint)
+migrate = Migrate()
+login_ = LoginManager()
+login_.login_view = 'user.login'
+bootstrap = Bootstrap()
 
-from webapp import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_.init_app(app)
+    bootstrap.init_app(app)
+
+    from webapp.parsing.views import blueprint as parsing_blueprint
+    app.register_blueprint(parsing_blueprint)
+
+    from webapp.search.views import blueprint as search_blueprint
+    app.register_blueprint(search_blueprint)
+
+    from webapp.user.views import blueprint as user_blueprint
+    app.register_blueprint(user_blueprint)
+
+    from webapp.user.models import User
+    @login_.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
--- webapp/parsing/views.py
+++ webapp/parsing/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template
 from webapp.parsing.bc_parsing import bandcamp_parsing
 from webapp.parsing.sc_parsing import soundcloud_parsing
 from webapp.parsing.models import Bandcamp, SoundCloud
@@ -10,9 +10,10 @@ blueprint = Blueprint('parsing', __name__)
 
 @blueprint.route('/')
 def parsing():
-    form=LoginForm()
+    form = LoginForm()
     bandcamp_parsing()
     soundcloud_parsing()
     popular_sc = SoundCloud.query.limit(8).all()
     popular_bc = Bandcamp.query.limit(20).all()
-    return render_template('index.html', popular_sc=popular_sc, popular_bc=popular_bc, form=form)
+    return render_template('index.html', popular_sc=popular_sc,
+                           popular_bc=popular_bc, form=form)

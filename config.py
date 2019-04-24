e6dd7e538ee63b4ac8dbadc3e12a0599ff6506b8
nemcev <alexnemtsev@mail.ru>
2019-04-24 21:17:29 +0300

    Revert "рефакторинг (не подключен postgres)"
    This reverts commit 7f4cc4d1ee2a2a72b2bcbfa5fbfb29ef3fba284e.

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            #'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:miro1234@localhost/flaskdbproject'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:miro1234@localhost/flaskdbproject'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SC_API_MAINPAGE = "https://api-v2.soundcloud.com/featured_tracks/front?client_id=z7npDMrLmgiW4wc8pPCQkkUUtRQkWZOF&limit=20&offset=0&linked_partitioning=1&app_version=1553260698&app_locale=en"
     BC_API = [

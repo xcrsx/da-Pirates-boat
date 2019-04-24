b8a23cf393ffc68960cd3e618377e7870a3a2bb4
nemcev <alexnemtsev@mail.ru>
2019-04-24 21:19:30 +0300

    Revert "рефакторинг (не подключен postgres)"
    This reverts commit 7f4cc4d1ee2a2a72b2bcbfa5fbfb29ef3fba284e.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,29 +4,21 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from webapp.parsing.views import blueprint as parsing_blueprint
 from webapp.user.views import blueprint as user_blueprint
-from webapp.user.models import User
 from webapp.search.views import blueprint as search_blueprint
 from webapp.db import db
 from flask_bootstrap import Bootstrap
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)    
-    migrate = Migrate(app, db)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    login = LoginManager(app)
-    login.login_view = 'user.login'
+db.init_app(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'user.login'
+bootstrap = Bootstrap(app)
+app.register_blueprint(parsing_blueprint)
+app.register_blueprint(search_blueprint)
+app.register_blueprint(user_blueprint)
 
-    bootstrap = Bootstrap(app)
-
-    app.register_blueprint(parsing_blueprint)
-    app.register_blueprint(search_blueprint)
-    app.register_blueprint(user_blueprint)
-
-    @login.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    return app
+from webapp import routes

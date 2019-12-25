21d93ab32ddacd621bb7396597868fc9ce539bec
Vitaly Kalinin <xcrsx@icloud.com>
2019-12-25 17:33:55 +0530

    changed user_id

--- webapp/search/search_in_sc.py
+++ webapp/search/search_in_sc.py
@@ -10,12 +10,13 @@ def search_sc():
         'sc_a_id': '3f437705c7fa502c1d352917a83b95768acbe00a',
         'variant_ids': '',
         'facet': 'model',
-        'user_id': '57289-61909-388511-64921',
-        'client_id': '9yZSvlXAK7Wmu4xhb0hdMtjP9D2z351X',
+        'user_id': '364967-67608-407779-700571',
+        'client_id': 'YUKXoArFcqrlQn9tfNHvvyfnDISj04zk',
     }
+    # limit=20&offset=0&linked_partitioning=1&app_version=1576752087&app_locale=en
     r = requests.get(url, data)
-    r = r.json()
     search_result = []
+    r = r.json()
     for result in r['collection']:
         url_song = result['permalink_url']
         search_result.append({'song': url_song})
